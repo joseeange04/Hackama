dd08c78b1221c8afafa727c3961604f26436bf5c
joseeange04 <ravololonirinap19.aps1a@gmail.com>
2023-02-08 03:33:07 +0100

    Ajout de langue et menu persistant

--- core.py
+++ core.py
@@ -1,26 +1,54 @@
 import ampalibe
-from conf import Configuration
+from ampalibe import Messenger, Payload, translate, Model
+from ampalibe.ui import QuickReply, Button, Type
 
-bot = ampalibe.init(Configuration())
-
-# create a get started option to get permission of user.
-# bot.chat.get_started()
+chat = Messenger()
+query = Model()
 
 @ampalibe.command('/')
-def main(sender_id, cmd, **extends):
-    '''
-    main function where messages received on
-    the facebook page come in.
+def main(sender_id, lang, cmd, **extends):
+    langage = [
+        QuickReply(
+            title = 'Malagasy',
+            payload = Payload('/mg')
+        ),
+        QuickReply(
+            title = 'Français',
+            payload = Payload('/fr')
+        ),
+        QuickReply(
+            title = 'Anglais',
+            payload = Payload('/en')
+        )
+    ]
+    chat.send_quick_reply(sender_id, langage, "Français:Pour commencez choisissez votre langue./ Malagasy: Safidio aary ny fiteny ampiasainao./ English: Please choose your language.")
+    
+    chat.send_text(sender_id, translate("Merci pour votre choix", lang))
 
-    @param sender_id String: 
-        sender facebook id
-    @param cmd String: 
-        message content
-    @param extends Dict: 
-        contain list of others
-            data sent by facebook (sending time, ...)
-            data sent by your payload if not set in parameter
-    '''
-    
-    bot.chat.send_message(sender_id, "Hello, Ampalibe")
-    
+    persistent_menu = [
+        Button(
+            type = Type.postback,
+            title = translate("Information sur les vaccins", lang),
+            payload = Payload('/information')
+        ),
+        Button(
+            type = Type.postback,
+            title = translate("Prochaine festival vacciné", lang),
+            payload = Payload('/festival')
+        ),
+        Button(
+            type = Type.postback,
+            title = "Statistique de vaccination contre covid-19",
+            payload = Payload('/statistique')
+        ),
+        Button(
+            type = Type.postback,
+            title = "Destination touristique sécurisé",
+            payload = Payload('/destination')
+        )
+
+    ]
+    chat.persistent_menu(sender_id, persistent_menu)
+
+
+
