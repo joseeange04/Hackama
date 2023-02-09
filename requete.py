5ac06acacf51ac9084f9246e9ce68e0d86a82ada
joseeange04 <ravololonirinap19.aps1a@gmail.com>
2023-02-10 00:33:10 +0100

    Add vacc query

--- requete.py
+++ requete.py
@@ -1,4 +1,21 @@
 from ampalibe import Model
+class Requete (Model):
+    def __init__(self, conf):
+        """
+            Connexion à notre base de donnée
+        """
+        Model(self, conf)
 
-Class Requete (Model):
-def 
+    @Model.verif_db
+    def Get_Vaccination_list(self):
+        """
+        Recupérer la liste des vaccins existants
+        """
+        req = """
+                SELECT id_vaccin, nom_vaccin, illustration, description 
+                FROM vaccins
+        """
+        self.cursor.execute(req)
+        result = self.cursor.fetchall()
+        self.db.commit()
+        return result
